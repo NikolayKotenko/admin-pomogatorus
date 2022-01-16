36134c8b71f67938b4ee7c44f48050e604fba2b7
equarmx <equarmx@gmail.com>
2022-01-16 21:49:38 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header/>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Header from "./components/Header";
+export default {
+  name: 'App',
+  components: {Header},
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-app-bar
+        dense
+        dark
+        clipped-left
+        app
+    >
+      <template v-if="drawer">
+        <v-icon small>
+          mdi-arrow
+        </v-icon>
+      </template>
+      <template v-else>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+        <v-chip
+            color="white"
+            text-color="primary"
+            style="cursor: pointer"
+        >
+          <v-icon small left>
+            mdi-briefcase
+          </v-icon>
+          Рабочий стол
+        </v-chip>
+
+      <v-spacer></v-spacer>
+
+      <v-menu
+          left
+          bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        clipped
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
