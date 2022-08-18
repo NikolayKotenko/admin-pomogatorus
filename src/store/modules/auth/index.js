cc9624cf6f11cb3814d2d0ac774d60f0e538a321
nikolay-kotenko <nikolay-kotenko@mail.ru>
2022-08-18 23:21:39 +0500

    - Правки формы авторизации

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -32,7 +32,7 @@ export default {
 
             return tokensData
         },
-        async createAndAuthUserByEmail(_, objData) {
+        async createUserByEmail(_, objData) {
             //Делаем запрос на создание пользователя, если такой есть то будет 409 конфликт ошибка ну и бог с ней
             return await Request.post(this.state.BASE_URL+'/users/create-from-only-email', objData)
         },
